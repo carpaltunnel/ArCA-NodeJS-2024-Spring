a08ed27c096e872cfecb1d68ff3f5f2790ca0a29
CC <C@C.com>
2024-02-14 17:36:58 -0600

    params

---
+++ 021-Parameters/code/argv.js
@@ -0,0 +1,14 @@
+console.log(process.argv);
+
+
+/*
+// Easy example of retrieving "named" parameters
+const userParam = '--username';
+const passParam = '--password';
+
+const username = process.argv[process.argv.indexOf(userParam) + 1];
+const password = process.argv[process.argv.indexOf(passParam) + 1];
+
+console.log(`Username : ${username}`);
+console.log(`Password : ${password}`);
+*/
