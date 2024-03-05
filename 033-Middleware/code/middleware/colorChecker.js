2268ff6fb8d35c0fe45b06d95afb92ba42dab6ce
CC <C@C.com>
2024-03-04 20:39:08 -0600

    middleware

---
+++ 033-Middleware/code/middleware/colorChecker.js
@@ -0,0 +1,19 @@
+
+const middleware = () => {
+  return (req, res, next) => {
+    if (req.body && req.body.color) {
+      console.log(`Found a color of ${req.body.color}`);
+
+      if (req.body.color === 'orange') {
+        res.status(400).send('We do not support orange widgets!');
+        return;
+      }
+
+      // Modify request body
+    }
+    
+    next();
+  };
+}
+
+module.exports = middleware;
